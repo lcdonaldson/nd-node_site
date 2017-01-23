bcec555918f46340bd3e3b6ce133e4950bc2e7d2
Levi Donaldson <lcdbacanas@gmail.com>
2017-01-22 20:55:12 -0600

    added static content

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
-var express = require('express');
-var app = express();
-var port = 8080;
-
-// route our app
-// var router = require('./app/routes');
-// app.use('/', router);
-
-
+// require our dependencies
+var express        = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var bodyParser     = require('body-parser');
+var app            = express();
+var port           = process.env.PORT || 8080;
 
 // use ejs and express layouts
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
-// set static files location
+// use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// route our app
+var router = require('./app/routes');
+app.use('/', router);
+
+// set static files (css and images, etc) location
+app.use(express.static(__dirname + '/public'));
 
 // start the server
-
-
-app.listen(port, function () {
-    console.log('app started');
+app.listen(port, function() {
+  console.log('app started');
 });
-
-app.get('/', function (req, res) {
-    res.send('hello again');
-});
-
-// var express = require('express');
-// var expressLayouts = require('express-ejs-layouts');
-// var app = express();
-// var port = 8080;
-
-// // route our app
-// var router = require ('./app/routes');
-// app.use('/', router);
-
-// // use ejs and express layouts
-// app.set('view engine', 'ejs');
-// app.use(expressLayouts);
-
-// // set static files (css and images, etc) location
-// app.use(express.static(__dirname + '/assets'));
-
-// //start the server
-// app.listen(port, function () {
-//     console.log('app started');
-// });
